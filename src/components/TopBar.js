a8c840b97e248096208564e74f2d02e850d1db12
Jonaspng <jonaspng@hotmail.com>
2023-01-02 14:52:29 +0800

    Remove theme

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -9,8 +9,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
-import { ThemeProvider } from '@emotion/react';
-import Theme from '../Theme';
 
 const pages = [{title: 'About Me', id: 'aboutme'},{title: 'Recent Activity', id:'recentactivity'}, 
 {title: 'Projects', id:'projects'}, {title:'History', id:'history'}];
@@ -29,98 +27,92 @@ function Topbar() {
   };
 
   return (
-    <div>
-      <ThemeProvider theme={Theme}>
-        <AppBar position="static" color='dark'>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href="/"
-                sx={{
-                  mr: 2,
-                  display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-              >
-                Jonas
-              </Typography>
+    <AppBar position="static" sx={{backgroundColor: 'black'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Jonas
+          </Typography>
 
-              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                  size="large"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: 'block', md: 'none' },
-                  }}
-                >
-                  {pages.map((page) => (
-                    <MenuItem key={page.title} onClick={() => handleCloseNavMenu(page.id)}>
-                      <Typography textAlign="center">{page.title}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Box>
-              <Typography
-                variant="h5"
-                noWrap
-                component="a"
-                href=""
-                sx={{
-                  mr: 2,
-                  display: { xs: 'flex', md: 'none' },
-                  flexGrow: 1,
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.title} onClick={() => handleCloseNavMenu(page.id)}>
+                  <Typography textAlign="center">{page.title}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Jonas
+          </Typography>
+          <Box sx={{ display: { xs: 'none', md: 'flex'}, marginLeft:'auto'  }}>
+            {pages.map((page) => (
+              <Button
+                key={page.title}
+                onClick={() => handleCloseNavMenu(page.id)}
+                sx={{ my: 2, color: 'white', display: 'block'}}
               >
-                Jonas
-              </Typography>
-              <Box sx={{ display: { xs: 'none', md: 'flex'}, marginLeft:'auto'  }}>
-                {pages.map((page) => (
-                  <Button
-                    key={page.title}
-                    onClick={() => handleCloseNavMenu(page.id)}
-                    sx={{ my: 2, color: 'white', display: 'block'}}
-                  >
-                    {page.title}
-                  </Button>
-                ))}
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </ThemeProvider>
-    </div>
-    
-    
+                {page.title}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar> 
   );
 }
 export default Topbar;
