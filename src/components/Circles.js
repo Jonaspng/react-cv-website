1bbf27d17aee82a646029abf5df381ea4453a8ef
Jonaspng <jonaspng@hotmail.com>
2023-01-15 23:05:54 +0800

    Add background css animation

---
+++ src/components/Circles.js
@@ -0,0 +1,19 @@
+
+function Circles() {
+
+  return (
+    <div className="circles">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+
+}
+
+export default Circles;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutMe from '../components/AboutMe';
 import BottomBar from '../components/BottomBar';
+import Circles from '../components/Circles';
 import History from '../components/History';
 import Projects from '../components/Projects';
 import RecentActivity from '../components/RecentActivity';
@@ -11,10 +12,14 @@ function MainPage() {
   return (
     <>
       <Topbar />
-      <AboutMe />
-      <RecentActivity />
-      <Projects />
-      <History />
+      <div>
+        <AboutMe />
+        <RecentActivity />
+        <Projects />
+        <History />
+        <Circles />
+      </div>
+      
       <BottomBar />
     </>
     
