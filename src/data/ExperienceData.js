98edb6fbc7b702cac2b28ee04c3592785c5bf6ec
Jonaspng <jonaspng@hotmail.com>
2024-08-23 16:09:20 +0800

    Update ExperienceData.js

--- src/data/ExperienceData.js
+++ src/data/ExperienceData.js
@@ -1,10 +1,18 @@
 const ExperienceData = [
   { title:"Software Engineer Intern",
+    location: "Synaxpe",
+    date: "June 2024 - Aug 2024",
+    descriptions:["Experimented with AWS AI tools in AWS Bedrock.",
+                "Designed a GEN AI POC that uses Retrieval Augmented Generation (RAG) to produce context aware chatbot for patients."],
+    technologies:"Python, Streamlit, AWS Bedrock, AWS Lambda, Anthropic Claude",
+  },
+  { title:"Software Engineer Intern",
     location: "Land Transport Authority (LTA)",
     date: "May 2023 - Aug 2023",
     descriptions:["Developed additional features for a LTA web application to streamline the approval and deployment processes for over 100 LTA applications used by over 1000 LTA Employees.",
                 "Designed and implemented UI/UX for internal LTA website viewed by more than 1000 LTA employees."],
-    technologies:"ReactJS, Bootstrap, AWS, DynamoDB, yarn",
-}]
+    technologies:"ReactJS, Bootstrap, AWS, AWS DynamoDB, yarn",
+  }
+]
 
 export default ExperienceData;
